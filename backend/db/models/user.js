a587c9e9ce10d3fc47992520b15c59958ea9c6c4
Thomas Yuan <thomasyuan478@gmail.com>
2023-07-07 14:38:13 -0500

    add firstName and lastName to migration, model, and routes

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -11,6 +11,18 @@ module.exports = (sequelize, DataTypes) => {
 
   User.init(
     {
+      firstName: {
+        type: DataTypes.STRING,
+        validate: {
+          isAlpha: true
+        }
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        validate: {
+          isAlpha: true
+        }
+      },
       username: {
         type: DataTypes.STRING,
         allowNull: false,
--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -48,6 +48,8 @@ router.post(
       id: user.id,
       email: user.email,
       username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName
     };
 
     await setTokenCookie(res, safeUser);

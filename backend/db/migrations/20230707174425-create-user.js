becefa30ca9f50c873cc4932845c10d5cd27e9d4
Thomas Yuan <thomasyuan478@gmail.com>
2023-07-12 15:02:17 -0500

    add options object to migrations

--- backend/db/migrations/20230707174425-create-user.js
+++ backend/db/migrations/20230707174425-create-user.js
@@ -6,6 +6,12 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
